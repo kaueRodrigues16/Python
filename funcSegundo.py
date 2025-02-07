1028f2fe351169805040b56355eeaa812f3f4aa9
Triaguin2 <133519497+Triaguin@users.noreply.github.com>
2025-02-06 22:04:16 -0300

    Update funcSegundo.py

--- funcSegundo.py
+++ funcSegundo.py
@@ -1,3 +1,4 @@
+import math
 print("insira um valor de X napar função X^2+5")
 
 X= int(input())
@@ -10,6 +11,44 @@ def medio():
     resolucao = (simples(X)-5)/X
     return(resolucao)
 
+def desenharEquacacao(A,B,C):
+    if A < 0:
+        a = " - " + str(-A)
+    elif A > 0:
+        a = str(A)
+    if B > 0:
+        b = " + " + str(B)
+    elif B < 0:
+        b = " - " + str(-B)
+    if C > 0:
+        c = " + " + str(C)
+    elif C < 0:
+        c = " - " + str(-C)
+
+    desenhoDaFormula = " F(x) = " + a + "X^2 " + b + "X " + c
+    return (desenhoDaFormula)
+
+def raizesdaFunc():
+    print("Insira um valor para A")
+    A = int(input())
+
+    print("Insira um valor para B")
+    B = int(input())
+
+    print("Insira um valor para C")
+    C = int(input())
+
+    print(desenharEquacacao(A, B, C))
+    Delta = B*B - 4*A*C
+
+    x1 = ((-B) + math.sqrt(Delta)) / 2 * A
+    x2 = ((-B) - math.sqrt(Delta)) / 2 * A
+
+    resultado = "Delta = ", Delta , " X1 = ", x1 , " X2 = ",x2
+    return (resultado)
+
+
 
 print(simples(X))
 print(medio())
+print(raizesdaFunc())
